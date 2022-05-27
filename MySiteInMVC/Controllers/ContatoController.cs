587f94c6a3ea0d5b9951be407d4d75f0cd545eba
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-05-26 21:46:56 -0300

    Create Button Delete and Update

--- MySiteInMVC/Controllers/ContatoController.cs
+++ MySiteInMVC/Controllers/ContatoController.cs
@@ -9,5 +9,23 @@ namespace MySiteInMVC.Controllers
 
             return View();
         }
+
+        public IActionResult Criar()
+        {
+
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+
+            return View();
+        }
     }
 }
