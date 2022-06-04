b5ccf3f35ac4d2abaf2ef222f80c92d25099c692
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-06-04 12:38:33 -0300

    Filters

--- MySiteInMVC/Controllers/ContatoController.cs
+++ MySiteInMVC/Controllers/ContatoController.cs
@@ -1,49 +1,104 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MySiteInMVC.Filters;
+using Microsoft.AspNetCore.Mvc;
 using MySiteInMVC.Models;
 using MySiteInMVC.Repository;
+using System;
 using System.Collections.Generic;
 
 namespace MySiteInMVC.Controllers
 {
-    
-    public class ContatoController : Controller
 
+    [PaginaParaUsuarioLogado]
+    public class ContatoController : Controller
     {
         private readonly IContatoRepository _contatoRepository;
+
         public ContatoController(IContatoRepository contatoRepository)
         {
             _contatoRepository = contatoRepository;
         }
+
         public IActionResult Index()
         {
             List<ContatoModel> contatos = _contatoRepository.BuscarTodos();
-                
+
             return View(contatos);
         }
 
         public IActionResult Criar()
         {
-
             return View();
         }
 
-        public IActionResult Editar(int Id)
+        public IActionResult Editar(int id)
         {
-
-            return View();
+            ContatoModel contato = _contatoRepository.BuscarPorID(id);
+            return View(contato);
         }
 
-        public IActionResult ApagarConfirmacao()
+        public IActionResult ApagarConfirmacao(int id)
         {
+            ContatoModel contato = _contatoRepository.BuscarPorID(id);
+            return View(contato);
+        }
 
-            return View();
+        public IActionResult Apagar(int id)
+        {
+            try
+            {
+                bool apagado = _contatoRepository.Apagar(id);
+
+                if (apagado) TempData["MensagemSucesso"] = "Contato apagado com sucesso!"; else TempData["MensagemErro"] = "Ops, não conseguimos cadastrar seu contato, tente novamante!";
+                return RedirectToAction("Index");
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar seu contato, tente novamante, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
 
         [HttpPost]
         public IActionResult Criar(ContatoModel contato)
         {
-            _contatoRepository.Adicionar(contato);
-            return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contato = _contatoRepository.Adicionar(contato);
+
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(contato);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu contato, tente novamante, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ContatoModel contato)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    contato = _contatoRepository.Atualizar(contato);
+                    TempData["MensagemSucesso"] = "Contato alterado com sucesso!";
+                    return RedirectToAction("Index");
+                }
+
+                return View(contato);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos atualizar seu contato, tente novamante, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
         }
     }
 }
